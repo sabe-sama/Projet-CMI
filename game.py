cdfa8dc84ef773cceb4d786d3378f1e8856bf206
sabe-sama <98398161+sabe-sama@users.noreply.github.com>
2022-02-10 21:09:17 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+#création d'une classe qui va représenter le jeu
+class Game :
+
+    def __init__(self):
+        #définir si le jeu a commencé ou non
+        self.is_playing= False
